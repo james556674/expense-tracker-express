1537fb3f69b6916d902928b5b567e72bbc5375c0
James <james556674@gmail.com>
2021-03-02 18:51:00 +0800

    feat: add bootstrap and add main page

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 const express = require('express')
+const exphbs = require('express-handlebars')
 
 const app = express()
 
 const PORT = 3000
 
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+
 app.get('/', (req, res) => {
-  res.send('this is a expense tracker')
+  const records = [
+    {
+      category: '餐飲',
+      name: '午餐',
+      date: '2020/10/16',
+      amount: 50
+    }
+  ]
+  res.render('index', { totalAmount: 1000, records })
 })
 
 app.listen(PORT, () => {
